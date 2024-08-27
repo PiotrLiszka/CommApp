9060890c1d49ae4874e86a9edb68b8e4dd2d8c82
PiotrLiszka <piotr.liszka.krk@gmail.com>
2024-08-27 23:29:43 +0200

    connection working

--- commapp-cs/CommunicationsLib/Sender.cs
+++ commapp-cs/CommunicationsLib/Sender.cs
@@ -1,31 +1,45 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
-using static System.Console;
 
 namespace CommunicationsLib;
 
-public static class Sender
-{
-    public static void SendMessage(string messageToSend)
-    {
-        ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+public class Sender
+{ 
+    private readonly ConnectionFactory factory;
+    private readonly IConnection connection;
+    private readonly IModel channel;
 
+    public Sender(bool localHost = true)
+    {
+        if (localHost)
+        {
+            factory = new ConnectionFactory { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+        }
+        else
+        {
+            //  TODO: ip port username i password z pliku 
+        }
+    }
+
+    public void SendMessage(string messageToSend)
+    {
         channel.QueueDeclare(queue: "hello",
-                     durable: false,
-                     exclusive: false,
-                     autoDelete: false,
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
                      arguments: null);
 
-        var message = "Hello World?";
-        var body = Encoding.UTF8.GetBytes(message);
+
+        var body = Encoding.UTF8.GetBytes(messageToSend);
 
         channel.BasicPublish(exchange: string.Empty,
                              routingKey: "hello",
                              basicProperties: null,
                              body: body);
-        Console.WriteLine($" [x] Sent {message}");
+
+        Console.WriteLine($" [x] Sent {messageToSend}");
 
         Console.WriteLine(" Press [enter] to exit.");
     }
